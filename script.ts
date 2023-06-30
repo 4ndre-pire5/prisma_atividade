3ccb0e5e9a4193e23dc3d5c626afde43bf93fddd
Andre Pires <andre.figueiredo@aluno.faculdadeimpacta.com.br>
2023-06-29 21:59:51 -0300

    subindo com um erro

--- script.ts
+++ script.ts
@@ -3,21 +3,55 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function main() {
+
+  let allAuthor = await prisma.author.findMany()
+  console.log(allAuthor)
+
+  allAuthor.forEach(async (p) => {
+      const deletedPosts = await prisma.author.delete({
+        where: {
+          id: p.id,
+        }
+      })
+    })
+
+    let allUser = await prisma.user.findMany()
+    console.log(allUser)
+
+  allUser.forEach(async (p) => {
+      const deletedPosts = await prisma.user.delete({
+        where: {
+          id: p.id,
+        }
+      })
+    })
     
     const user = await prisma.user.create({
         data: {
-          name: '',
-          email: '',
+          name: 'Andre',
+          email: 'andre@teste.prisma.io',
           authors: {
             create: {
               tags: '',
-              surname: '',
-              completeName: '',
+              surname: 'Pires',
+              completeName: 'de Figueiredo',
             },
-          },       
+          },
+            posts: {
+              create: [
+                {title: 'Teste'},
+                {text: 'Exercicio Prisma'},
+              ],
+            },
+          comments: {
+            create: [
+              {text: ''},
+            ],
+          },     
         },
     })
-    console.log(user)
+ 
+   console.log(user)
 
 }
 
